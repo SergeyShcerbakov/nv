ba1a38209faaaaf3ea3c98df4ff6c53f772bdea2
Your Name <you@example.com>
2022-08-19 11:58:01 +0300

    add folder

---
+++ gulpfile.js
@@ -0,0 +1,62 @@
+const gulp = require('gulp');
+const fileinclude = require('gulp-file-include');
+const sass = require('gulp-sass')(require('sass'));
+const cssnano = require("gulp-cssnano");
+const { series } = require("gulp");
+
+function defaultTask(cb) {
+    // place code for your default task here
+    cb();
+}
+
+function htmlInckude() {
+    return gulp
+        .src(['app/index.html'])
+        .pipe(fileinclude({
+            prefix: '@@',
+            basepath: '@file'
+        }))
+        .pipe(gulp.dest('dest'));
+};
+
+function buildStyles() {
+    return gulp.src('app/scss/**/*.scss')
+        .pipe(sass.sync().on('error', sass.logError))
+        .pipe(gulp.dest('app/css'));
+};
+
+function cssNanotodest() {
+    return gulp
+        .src('app/css/*.css')
+        .pipe(cssnano())
+        .pipe(gulp.dest('dest/css'));
+};
+
+function imgmin() {
+    return gulp
+        .src('app/images/**/*.*')
+        .pipe(
+            imagemin([
+                imagemin.gifsicle({ interlaced: true }),
+                imagemin.mozjpeg({ quality: 80, progressive: true }),
+                imagemin.optipng({ optimizationLevel: 5 }),
+                imagemin.svgo({
+                    plugins: [{ removeViewBox: true }, { cleanupIDs: false }],
+                }),
+            ])
+        )
+        .pipe(gulp.dest("dest/images"));
+}
+
+exports.default = defaultTask; // gulp
+exports.html = htmlInckude;
+exports.scss = buildStyles;
+exports.css = cssNanotodest;
+exports.img = imgmin;
+
+exports.dev = series (
+    htmlInckude,
+    buildStyles,
+    cssNanotodest,
+    imgmin
+);
